a259f0faa58d8c27578e0aebcbf6888d5fd86088
Lucas Anderson <lucas.anderson@sportngin.com>
2014-12-17 15:51:13 -0600

    Rewrite

--- lib/prawn/qrcode.rb
+++ lib/prawn/qrcode.rb
@@ -26,39 +26,33 @@ require 'rqrcode'
 # *License*::   Apache License, Version 2.0
 #
 module QRCode
-
-  # The default size for QR Code modules is 1/72 in
-  DEFAULT_DOTSIZE = 1.to_f
-
   # Prints a QR Code to the PDF document. The QR Code creation happens on the fly.
   #
-  # content::  The string to render as content of the QR Code
+  # Arguments:
+  # <tt>content/tt>:: The string to render as content of the QR Code
   #
-  # *options:: Named optional parameters
-  #
-  #   +:level+:: Error correction level to use. One of: (:l,:m,:h,:q), Defaults to :m
-  #   +:extent+:: Size of QR Code given in pt (1 pt == 1/72 in)
-  #   +:pos+:: Two-element array containing the position at which the QR-Code should be rendered. Defaults to [0,cursor]
-  #   +:dot+:: Size of QR Code module/dot. Calculated from extent or defaulting to 1pt
-  #   +:stroke+:: boolean value whether to draw bounds around the QR Code.
-  #             Defaults to true.
-  #   +:align+:: Optional alignment within the current bounding box. Valid values are :left, :right, and :center. If set
-  #             This option overrides the horizontal positioning specified in :pos. Defaults to nil.
-  #
-  def print_qr_code(content, *options)
-    opt = options.extract_options!
-    qr_version = 0
-    level = opt[:level] || :m
-    extent = opt[:extent].to_f
-    dot_size = opt[:dot].to_f || DEFAULT_DOTSIZE
-    begin
-      qr_version +=1
-      qr_code = RQRCode::QRCode.new(content, :size=>qr_version, :level=>level)
+  # Options:
+  # <tt>:options</tt>:: Named optional parameters
+  # <tt>:version</tt>:: The QR Code version
+  # <tt>:level</tt>:: Error correction level to use. One of: (:l,:m,:h,:q), Defaults to :m
+  # <tt>:dot</tt>:: Size of QR Code module/dot.
+  # <tt>:at</tt>:: an array [x,y] with the location of the top left corner of the image.
+  # <tt>:position</tt>::  One of (:left, :center, :right) or an x-offset
+  # <tt>:vposition</tt>::  One of (:top, :center, :center) or an y-offset
+  # <tt>:height</tt>:: the height of the image [actual height of the image]
+  # <tt>:width</tt>:: the width of the image [actual width of the image]
+  # <tt>:scale</tt>:: scale the dimensions of the image proportionally
+  # <tt>:fit</tt>:: scale the dimensions of the image proportionally to fit inside [width,height]
+  def print_qr_code(content, options = {})
+    version = options[:version] || 0
+    level = options[:level] || :m
 
-      dot_size = extent/(8+qr_code.modules.length) if extent
-      render_qr_code(qr_code, opt.merge(:dot=>dot_size))
+    begin
+      version +=1
+      qr_code = RQRCode::QRCode.new(content, :size => version, :level=> level)
+      render_qr_code(qr_code)
     rescue RQRCode::QRCodeRunTimeError
-      if qr_version <40
+      if version <40
         retry
       else
         raise
@@ -66,73 +60,102 @@ module QRCode
     end
   end
 
-  # Renders a prepared QR Code (RQRCode::QRCode) object.
+  # Renders a QR Code (RQRCode::QRCode) object.
   #
-  # qr_code:: The QR Code (an RQRCode::QRCode) to render
+  # Arguments:
+  # <tt>qr_code/tt>:: The QR Code (an RQRCode::QRCode) to render
   #
-  # *options:: Named optional parameters
-  #   +:extent+:: Size of QR Code given in pt (1 pt == 1/72 in)
-  #   +:pos+:: Two-element array containing the position at which the QR-Code should be rendered. Defaults to [0,cursor]
-  #   +:dot+:: Size of QR Code module/dot. Calculated from extent or defaulting to 1pt
-  #   +:stroke+:: boolean value whether to draw bounds around the QR Code. Defaults to true.
-  #   +:align+:: Optional alignment within the current bounding box. Valid values are :left, :right, and :center. If set
-  #             This option overrides the horizontal positioning specified in :pos. Defaults to nil.
-  def render_qr_code(qr_code, *options)
-    opt = options.extract_options!
-    dot = opt[:dot].to_f || DEFAULT_DOTSIZE
-    extent= opt[:extent].to_f || (8+qr_code.modules.length) * dot
-    stroke = true
-    stroke = opt[:stroke] if opt.has_key?(:stroke)
+  # Options:
+  # <tt>:options</tt>:: Named optional parameters
+  # <tt>:dot</tt>:: Size of QR Code module/dot.
+  # <tt>:at</tt>:: an array [x,y] with the location of the top left corner of the image.
+  # <tt>:position</tt>::  One of (:left, :center, :right) or an x-offset
+  # <tt>:vposition</tt>::  One of (:top, :center, :center) or an y-offset
+  # <tt>:height</tt>:: the height of the image [actual height of the image]
+  # <tt>:width</tt>:: the width of the image [actual width of the image]
+  # <tt>:scale</tt>:: scale the dimensions of the image proportionally
+  # <tt>:fit</tt>:: scale the dimensions of the image proportionally to fit inside [width,height]
+  def render_qr_code(qr_code, options = {})
+    dot_w, dot_h = qrcode_dot_size(qr_code, options)
+    if options[:at]
+      pos_x, pos_y = options[:at]
+    else
+      pos_x, pos_y = qrcode_position(qr_code.modules.first.length * dot_w, qr_code.modules.length * dot_h, options)
+    end
 
-    foreground_color = opt[:foreground_color] || '000000'
-    background_color = opt[:background_color] || 'FFFFFF'
-    stroke_color = opt[:stroke_color] || '000000'
+    qr_code.modules.each do |col|
+      temp_x = pos_x
+      col.each do |row|
+        if row
+          fill { rectangle([temp_x, pos_y], dot_w, dot_h) }
+        end
+        temp_x += dot_w
+      end
+      pos_y -= dot_h
+    end
+  end
 
-    pos = opt[:pos] ||[0, cursor]
-
-    align = opt[:align]
-    case(align)
+private
+  def qrcode_position(w, h, options = {})
+    options[:vposition] ||= :top
+    options[:position] ||= :left
+    
+    y = case options[:vposition]
+    when :top
+      bounds.top
     when :center
-      pos[0] = (@bounding_box.right / 2) - (extent / 2)
-    when :right
-      pos[0] = @bounding_box.right - extent
+      bounds.top - (bounds.height - h) / 2.0
+    when :bottom
+      bounds.bottom + h
+    when Numeric
+      bounds.top - options[:vposition]
+    end
+
+    x = case options[:position]
     when :left
-      pos[0] = 0;
+      bounds.left
+    when :center
+      bounds.left + (bounds.width - w)/ 2.0
+    when :right
+      bounds.right - w
+    when Numeric
+      options[:position] + bounds.left
     end
 
-    stroke_color stroke_color
-    fill_color background_color
+    return [x,y]
+  end
 
-    bounding_box pos, :width => extent, :height => extent do |box|
-      fill_color foreground_color
+  def qrcode_dot_size(qr_code, options)
+    qr_w = qr_code.modules.length
+    qr_h = qr_code.modules.first.length
+    w = options[:width] || qr_w
+    h = options[:height] || qr_h
 
-      if stroke
-        stroke_bounds
-      end
-
-      pos_y = 4*dot +qr_code.modules.length * dot
-
-      qr_code.modules.each_index do |row|
-        pos_x = 4*dot
-        dark_col = 0
-        qr_code.modules.each_index do |col|
-          move_to [pos_x, pos_y]
-          if qr_code.dark?(row, col)
-            dark_col = dark_col+1
-          else
-            if (dark_col>0)
-              fill { rectangle([pos_x - dark_col*dot, pos_y], dot*dark_col, dot) }
-              dark_col = 0
-            end
-          end
-          pos_x = pos_x + dot
-        end
-        if (dark_col > 0)
-          fill { rectangle([pos_x - dark_col*dot, pos_y], dot*dark_col, dot) }
-        end
-        pos_y = pos_y - dot
+    if options[:width] && !options[:height]
+      wp = w / qr_w
+      w = qr_w * wp
+      h = qr_h * wp
+    elsif options[:height] && !options[:width]
+      hp = h / qr_h
+      w = qr_w * hp
+      h = qr_h * hp
+    elsif options[:scale]
+      w = qr_w * options[:scale]
+      h = qr_h * options[:scale]
+    elsif options[:fit]
+      bw, bh = options[:fit]
+      bp = bw / bh.to_f
+      ip = qr_w / qr_h
+      if ip > bp
+        w = bw
+        h = bw / ip
+      else
+        h = bh
+        w = bh * ip
       end
     end
+
+    [w/qr_w, h/qr_h]
   end
 
 end
